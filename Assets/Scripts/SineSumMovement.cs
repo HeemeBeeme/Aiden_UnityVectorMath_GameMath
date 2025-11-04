80193b6912e5456ea650e92a08e18fbf3cf4b1c0
Heeme Beeme <heemebeeme@gmail.com>
2025-11-04 15:29:41 -0400

    scene 1 complete

--- Assets/Scripts/SineSumMovement.cs
+++ Assets/Scripts/SineSumMovement.cs
@@ -5,11 +5,6 @@ public class SineSumMovement : MonoBehaviour
     public GameObject Cube1;
     public GameObject Cube2;
 
-    [Header("Axis")]
-    public bool Xaxis = false;
-    public bool Yaxis = false;
-    public bool Zaxis = false;
-
     [Header("Cube 1 Values")]
     public float Cube1X;
     public float Cube1Y;
@@ -25,7 +20,7 @@ public class SineSumMovement : MonoBehaviour
     public float Yposition = 0;
     public float Zposition = 0;
 
-    private void Start()
+    void Update()
     {
         Cube1X = Cube1.transform.position.x;
         Cube1Y = Cube1.transform.position.y;
@@ -34,25 +29,12 @@ public class SineSumMovement : MonoBehaviour
         Cube2X = Cube2.transform.position.x;
         Cube2Y = Cube2.transform.position.y;
         Cube2Z = Cube2.transform.position.z;
-    }
 
-    void Update()
-    {
-        if(Xaxis)
-        {
-            Xposition = Cube1X + Cube2X;
-        }
-        else if (Yaxis)
-        {
-            Yposition = Cube1Y + Cube2Y;
-        }
-        else if (Zaxis)
-        {
-            Zposition = Cube1Z + Cube2Z;
-        }
-        else
-        {
-            Yaxis = true;
-        }
+        Xposition = Cube1X + Cube2X;
+        Yposition = Cube1Y + Cube2Y;
+        Zposition = Cube1Z + Cube2Z;
+
+        transform.position = new Vector3(Xposition, Yposition, Zposition);
+
     }
 }
